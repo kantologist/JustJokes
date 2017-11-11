46584696dea5f283b3ff632d0affab0faff573fd
kantologist <azeezfemi17937@yahoo.com>
2017-11-11 21:19:54 +0100

    Step3: created an android library with an activity for displaying jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.JokeClass;
+import com.example.androidjokes.JokesActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokesActivity.class);
+        JokeClass joke = new JokeClass();
+        intent.putExtra("joke", joke.tellJoke());
+        startActivity(intent);
     }
 
 
