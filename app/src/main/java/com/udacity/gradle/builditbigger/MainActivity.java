3af5a6df0e45dadad4ebe92f6193f7fe1a6c7abd
kantologist <azeezfemi17937@yahoo.com>
2017-11-12 03:42:06 +0100

    Step4: added functional test and refactored code to make it work

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -63,41 +63,4 @@ public class MainActivity extends AppCompatActivity {
         new EndpointsAsyncTask().execute(getApplicationContext());
 
     }
-
-    private class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
-        private MyApi myApiService = null;
-        private Context context;
-
-        @Override
-        protected String doInBackground(Context ... params) {
-            if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                        .setRootUrl("https://cohesive-memory-145712.appspot.com/_ah/api/");
-                // end options for devappserver
-
-                myApiService = builder.build();
-            }
-
-            context = params[0];
-
-
-            try {
-                return myApiService.myEndpoint().fetchJoke().execute().getJoke();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            progressBar.setVisibility(View.GONE);
-            Intent intent = new Intent(context, JokesActivity.class);
-            JokeClass joke = new JokeClass();
-            intent.putExtra("joke", joke.tellJoke());
-            startActivity(intent);
-
-        }
-    }
-
-
 }
